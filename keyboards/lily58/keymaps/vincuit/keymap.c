ac68c0119d6a70cf31781ff3d72cf027df8b608b
vincuit <94998571+vincuit@users.noreply.github.com>
2022-01-25 21:11:07 +0100

    Update keymap.c

--- keyboards/lily58/keymaps/vincuit/keymap.c
+++ keyboards/lily58/keymaps/vincuit/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| PLAY  |    |  DEL  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | NUMPD| CAPS | / Space /       \Enter \  |BackSP| RGUI |QWERTY|
+ *                   | LAlt | CAPS | NUMP | / Space /       \Enter \  |BackSP| RGUI |QWERTY|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -27,29 +27,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,   KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                        KC_LALT, TT(_NUMPAD), KC_CAPS, KC_SPC,  KC_ENT,  KC_BSPC, KC_RGUI, TO(_QWERTY)
+                        KC_LALT, KC_CAPS, TT(_NUMPAD), KC_SPC,  KC_ENT,  KC_BSPC, KC_RGUI, TO(_QWERTY)
 ),
 /* NUMPAD
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |      |      |      |      |      |      |                    |      |   /  |   *  |   -  |PRINTS| HOME |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |      |   /  |   *  |   -  |PRINTS| HOME |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |      |   7  |   8  |   9  |   +  | P_UP |
+ * |  F7  |  F8  |  F9  | F10  | F11  | F12  |                    |      |   7  |   8  |   9  |   +  | P_UP |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------.    ,-------|      |   4  |   5  |   6  |      | P_DN |
+ * |      |      |      |  UP  |      |      |-------.    ,-------|      |   4  |   5  |   6  |   =  | P_DN |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------|    |-------|   0  |   1  |   2  |   3  |   .  | END  |
+ * |      |      | LEFT | DOWN | RGHT |      |-------|    |-------|   0  |   1  |   2  |   3  |   .  | END  |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LAlt | NUMPD| CAPS | / Space /       \Enter \  |BackSP| RGUI |QWERTY|
+ *                   | LAlt | CAPS |QWERTY| / Space /       \Enter \  |BackSP| RGUI |QWERTY|
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
 
 [_NUMPAD] = LAYOUT(
-  _______, _______, _______, _______, _______, _______,                     _______, KC_PSLS, KC_PAST, KC_PMNS, KC_PSCR, KC_HOME,
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_PGUP,
-  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                      _______, KC_P4,   KC_P5,   KC_P6,   _______, KC_PGDN,
-  _______, _______, _______, _______, _______, _______,  _______, _______,  KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_END,
-                             _______, KC_TRNS, _______,  _______, _______,  _______, _______, KC_TRNS
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                       _______, KC_PSLS, KC_PAST, KC_PMNS, KC_PSCR, KC_HOME,
+  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                      _______, KC_P7,   KC_P8,   KC_P9,   KC_PPLS, KC_PGUP,
+  _______, _______, _______, KC_UP,   _______, _______,                     _______, KC_P4,   KC_P5,   KC_P6,   KC_PEQL, KC_PGDN,
+  _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,  _______, _______,  KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_PDOT, KC_END,
+                         _______, _______, TO(_QWERTY),  _______, _______,  _______, _______, KC_TRNS
 )
 };
 
@@ -118,11 +118,25 @@ bool oled_task_user(void) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
   /* With an if statement we can check which encoder was turned. */
   if (index == 0) { /* First encoder */
-    if (clockwise) {
+  /*  if (clockwise) {
     tap_code(KC_VOLU);
     } else {
         tap_code(KC_VOLD);
     }
+  }*/
+  if(IS_LAYER_ON(_NUMPAD)) { // on Numpad layer control volume
+    if (clockwise){
+      tap_code(KC_VOLU);
+    } else{
+      tap_code(KC_VOLD);
+    }        
+  } else { // on other layers emulate mouse scrollwheel
+    if (clockwise){
+      tap_code(KC_MS_WH_DOWN);
+    } else{
+      tap_code(KC_MS_WH_UP);
+    }
   }
   return false;
 }
+
